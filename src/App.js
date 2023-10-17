38811e254293ad072281190b1129057ea377e3c4
gasparamaral@gmail.com <gasparamaral@gmail.com>
2023-10-17 19:19:08 -0300

    css review and fetch imp

--- src/App.js
+++ src/App.js
@@ -3,36 +3,47 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-      posts: [
-          {
-            id: 1,
-            title: 'This is the title 1',
-            body: 'This is the body 1'
-          },
-          {
-            id: 2,
-            title: 'This is the title 2',
-            body: 'This is the body 2'
-          }, {
-            id: 3,
-            title: 'This is the title 3',
-            body: 'This is the body 3'
-          }
-        ]
-    };
+      posts: []
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const photosJson = await photos.json();
+    const postsJson = await posts.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
+
+    this.setState({ posts: postsAndPhotos});
+  }
 
   render () {
-    const {posts} = this.state;
+    const { posts } = this.state;
 
       return (   
-        <div className="App">
-          {posts.map(post => 
-            <div key={post.id}>
-              <h1>{post.title}</h1>
-              <p>{post.title} - {post.body}</p>
-            </div>
-          )}
-        </div>
+        <section className="container">
+          <div className="posts">
+            {posts.map(post => (
+              <div className="post">
+                <img src={post.cover} alt={post.title} />
+                <div key={post.id} className="post-content">
+                    <h1>{post.title}</h1>
+                    <p>{post.title} - {post.body}</p>
+                  </div>
+                </div>
+            ))}
+          </div>
+        </section>
       )
     }
   };
